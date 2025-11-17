5adfaffea07020f083e9946aebc978a050130004
rahulhere-1 <rmendigeri4@gmail.com>
2025-11-17 22:29:06 +0530

    initializing project

---
+++ web-crawler/src/test/java/com/example/web_crawler/WebCrawlerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.web_crawler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebCrawlerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
